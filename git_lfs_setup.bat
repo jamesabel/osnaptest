5f46a02bcd12b528e967be9546d406f849200c0c
james abel <j@abel.co>
2017-12-27 19:24:20 -0800

    add git lfs

--- git_lfs_setup.bat
+++ git_lfs_setup.bat
@@ -1,12 +1,9 @@
 REM should only have to do this once
 git lfs install
-git lfs track 'installers/*.exe'
-git lfs track 'installers/*.zip'
-REM look at what lfs is tracking
+git lfs track "installers/*.*"
+REM see what we did
 git lfs track
-REM add the files we want to put into github lfs
-git add .gitattributes "installers\*.exe"
-git add .gitattributes "installers\*.zip"
+git add .gitattributes
 git status
 git lfs ls-files
 REM now you can do
